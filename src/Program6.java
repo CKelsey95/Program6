04c348e96330e10c38e04cc57e0df95067dc9f20
Ckelsey95 <colton.e.kelsey@gmail.com>
2023-10-04 09:39:36 -0600

    Completed integer sorting and timing, need to implement string portion

--- src/Program6.java
+++ src/Program6.java
@@ -7,6 +7,9 @@ import java.util.*;
 
 public class Program6 {
 
+    public Program6() throws IOException {
+    }
+
     public static void main(String[] args) throws IOException {
         int index = 0;
         int bubbleArray[] = new int[20000];
@@ -25,11 +28,17 @@ public class Program6 {
         } catch (IOException e){
             System.out.println("Error reading the file: " + e.getMessage());
         }
-
-        bubbleSort(bubbleArray, index);
-
+    int n = bubbleArray.length;
+        bubbleSort(bubbleArray, n);
+        selectionSort(selectionArray);
+        // below is a test to confirm that the sorting is working correctly
+        BufferedWriter bubbleOutput = new BufferedWriter(new FileWriter("src\\bubOutput.txt"));
+            bubbleOutput.write(Arrays.toString(bubbleArray));
+        BufferedWriter selectionOutput = new BufferedWriter(new FileWriter("src\\selOutput.txt"));
+            selectionOutput.write(Arrays.toString(selectionArray));
     }
-    static void bubbleSort(int bubbleArray[], int n){
+    static void bubbleSort(int bubbleArray[],int n){
+        long timeStart = System.nanoTime();
         int i, j, temp;
         boolean swapped;
         for (i = 0; i < n - 1; i++) {
@@ -45,5 +54,26 @@ public class Program6 {
             if (swapped == false)
                 break;
         }
+        long timeEnd = System.nanoTime();
+        long bubbleTimeTotal = (timeEnd - timeStart);
+        System.out.println("total bubble time is: " + bubbleTimeTotal + " nanoseconds");
+    }
+    static void selectionSort(int selectionArray[]){
+       int n = selectionArray.length;
+       long timeStart = System.nanoTime();
+       for (int i = 0; i < n - 1; i++)
+       {
+           int min_idx = i;
+           for (int j = i + 1; j < n; j++)
+               if (selectionArray[j] < selectionArray[min_idx])
+                   min_idx = j;
+
+           int temp = selectionArray[min_idx];
+           selectionArray[min_idx] = selectionArray[i];
+           selectionArray[i] = temp;
+       }
+       long timeEnd = System.nanoTime();
+       long selectionTimeTotal = (timeEnd - timeStart);
+       System.out.println("total selection time is: " + selectionTimeTotal + " nanoseconds");
     }
 }
